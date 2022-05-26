897a3e4f28380495b4e9fb0d946973e4d6d62894
Denis <dent.90@mail.ru>
2022-05-26 09:37:19 +0300

    ДЗ по уроку 6. Добавлено использование MyBatis ORM для тестов создания и изменения продукта и после его удаления после выполнения теста.

--- mini-market-tests/src/test/java/CRUDProductTest.java
+++ mini-market-tests/src/test/java/CRUDProductTest.java
@@ -1,15 +1,23 @@
 import api.ProductService;
 import com.github.javafaker.Faker;
+import db.dao.CategoriesMapper;
+import db.dao.ProductsMapper;
+import db.model.Categories;
+import db.model.Products;
+import db.model.ProductsExample;
 import dto.Product;
 import okhttp3.ResponseBody;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.hamcrest.CoreMatchers;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import retrofit2.Response;
 import utils.RetrofitUtils;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -18,7 +26,10 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class CRUDProductTest {
 
-    static ProductService productService;
+    static ProductService productService = null;
+    static SqlSession sqlSession = null;
+    static ProductsMapper productsMapper = null;
+    static CategoriesMapper categoriesMapper = null;
     Faker faker = new Faker();
     Product product;
     Response<Product> response;
@@ -27,6 +38,18 @@ public class CRUDProductTest {
     @BeforeAll
     static void setUp() throws IOException {
         productService = RetrofitUtils.getRetrofit().create(ProductService.class);
+
+        SqlSessionFactory sqlSessionFactory;
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        sqlSession = sqlSessionFactory.openSession();
+    }
+
+    @BeforeEach
+    void createMappers(){
+        productsMapper = sqlSession.getMapper(ProductsMapper.class);
+        categoriesMapper = sqlSession.getMapper(CategoriesMapper.class);
     }
 
     @Test
@@ -43,6 +66,13 @@ public class CRUDProductTest {
         assertThat(response.body().getCategoryTitle(), containsStringIgnoringCase(product.getCategoryTitle()));
         assertThat(response.body().getPrice(), equalTo(product.getPrice()));
         idProduct = response.body().getId();
+
+        Products productInDB = productsMapper.selectByPrimaryKey(Long.valueOf(idProduct));
+        Categories productInDBCategory = categoriesMapper.selectByPrimaryKey(productInDB.getCategory_id());
+        assertThat(productInDB.getTitle(), containsStringIgnoringCase(product.getTitle()));
+        assertThat(productInDBCategory.getTitle(), containsStringIgnoringCase(product.getCategoryTitle()));
+        assertThat(productInDB.getPrice(), equalTo(product.getPrice()));
+        sqlSession.commit();
 
         response = productService.getProductById(idProduct).execute();
         assertThat(response.isSuccessful(), CoreMatchers.is(true));
@@ -63,6 +93,13 @@ public class CRUDProductTest {
         assertThat(response.body().getTitle(), containsStringIgnoringCase(product.getTitle()));
         assertThat(response.body().getCategoryTitle(), containsStringIgnoringCase(product.getCategoryTitle()));
         assertThat(response.body().getPrice(), equalTo(product.getPrice()));
+
+        productInDB = productsMapper.selectByPrimaryKey(Long.valueOf(idProduct));
+        productInDBCategory = categoriesMapper.selectByPrimaryKey(productInDB.getCategory_id());
+        assertThat(productInDB.getTitle(), containsStringIgnoringCase(product.getTitle()));
+        assertThat(productInDBCategory.getTitle(), containsStringIgnoringCase(product.getCategoryTitle()));
+        assertThat(productInDB.getPrice(), equalTo(product.getPrice()));
+        sqlSession.commit();
 
         Response<List<Product>> responseProductList = productService.getProducts().execute();
         assertThat(response.isSuccessful(), CoreMatchers.is(true));
@@ -87,5 +124,16 @@ public class CRUDProductTest {
                         .noneMatch(p -> p.equals(product)),
                 CoreMatchers.is(true)
         );
+
+        ProductsExample productExample = new ProductsExample();
+        productExample.createCriteria().andIdEqualTo(Long.valueOf(idProduct));
+        List<Products> productsList = productsMapper.selectByExample(productExample);
+        sqlSession.commit();
+        assertThat(productsList.size(), equalTo(0));
+    }
+
+    @AfterAll
+    static void tearDown(){
+        sqlSession.close();
     }
 }
